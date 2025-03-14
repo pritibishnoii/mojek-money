ce3a93cf31a8340292b5519fae58ad2137c494a2
priti <priti12bishnoi@gmail.com>
2025-03-14 22:12:28 +0530

    heading

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () =>
     return (
         <div className='w-full flex flex-col items-center justify-center h-max'>
             {/* Top Section */ }
-            <div className='h-48 w-full flex items-center justify-center'>
+            <div className='mt-28  w-full flex items-center justify-center '>
                 <div className='bg-white rounded-full w-[22%] h-max py-2 px-4 shadow-2xl flex gap-2 items-center'>
                     <span>⚡</span>
                     <span className='text-sm text-black'>New features out now!</span>
@@ -17,11 +17,11 @@ const Header = () =>
             </div>
 
             {/* Main Heading and Paragraph */ }
-            <div className='w-1/2 h-68 flex flex-col items-center justify-center text-center '>
-                <h1 className='text-8xl text-[#1f705d] font-bold '>
+            <div className='w-2/3  h-68 flex flex-col items-center justify-center text-center '>
+                <h1 className='text-7xl text-[#1f705d] font-semibold  leading-[1.2]'>
                     Save <span className='text-[#64f07c]'>Thousands</span> Every Single Month.
                 </h1>
-                <p className='text-[#9c9b9d] text-lg mt-4'>
+                <p className='text-[#9c9b9d] text-lg mt-4 w-3/4'>
                     Track smarter, spend better, and save thousands every month with Mojek’s personalized
                     financial insights.
                 </p>
